23d454f0346a978f68ed20977685f91a6f5d22cc
jlatorreaguilar <jesus.latorreaguilar@soprasteria.com>
2020-07-15 14:08:09 +0200

    errores pom y versión java

--- spring-sample-concurrency/concurrent-calls/src/main/java/com/nickolasfisher/concurrentcalls/ConcurrentRunner.java
+++ spring-sample-concurrency/concurrent-calls/src/main/java/com/nickolasfisher/concurrentcalls/ConcurrentRunner.java
@@ -1,17 +1,16 @@
 package com.nickolasfisher.concurrentcalls;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 @Component
 public class ConcurrentRunner implements CommandLineRunner {
